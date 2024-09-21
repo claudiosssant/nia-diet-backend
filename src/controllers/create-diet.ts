77587d81e932f4c96819bbeaab8483ace982b5da
Claudio <claudiogsssantos@gmail.com>
2024-09-21 16:34:54 -0300

    feat: creating interface

--- src/controllers/create-diet.ts
+++ src/controllers/create-diet.ts
@@ -1,8 +1,18 @@
 import { FastifyRequest, FastifyReply} from 'fastify'
 import { CreateDietService } from '../services/create-diet-service'
 
+
+interface UserDataProps {
+  age: string,
+  name: string,
+  weight: string,
+  height: string,
+  frequency: string,
+  objective: string,
+}
 class CreateDietController{
   async handle(request: FastifyRequest, reply: FastifyReply){
+    const {age, name, weight, height, frequency, objective} = request.body;
     
     const createDiet = new CreateDietService();
     const diet = await createDiet.create();
